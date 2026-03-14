aa85ee83f7c582b7426595ffd4c067f5d3ab2f0c
Pranita <pranitapanchal339@gmail.com>
2026-03-14 17:51:49 +0530

    feat: integrate login route with schema validation and controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller";
+import { registerUser, loginUser } from "../controllers/user.controller";
 import { validate } from "../middleware/validate.middleware";
-import { userSchema } from "../models/user.model";
+import { userSchema, loginSchema } from "../models/user.model";
 
 const router = Router();
 
 router.post("/register", validate(userSchema), registerUser);
+
+router.post("/login", validate(loginSchema), loginUser);
 
 export default router;
